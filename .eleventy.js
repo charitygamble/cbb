e375c58ab7b7f39a022d80bdfc1e1bc3945ba897
Charity Gamble <charity@charitygamble.dev>
2024-12-14 14:20:16 -0700

    Just made a couple changes

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: 'src',
+            output: 'dist',
             data: '_data',
             layouts: '_layouts',
             includes: '_includes'
